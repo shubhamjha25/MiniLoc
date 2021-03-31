36b924ed283fc0f668a0b67302849cfd5aefe614
Shubham Jha <shubham.jha2001m@gmail.com>
2021-03-31 20:20:54 +0530

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Link from "./components/Link/";
